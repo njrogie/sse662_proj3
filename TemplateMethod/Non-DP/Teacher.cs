f04d5f6b92359dfa0c2ada265c360849d53ae75d
Timothy Hood <tjhood97@gmail.com>
2019-10-29 19:30:32 -0400

    Added boilerplate for Non-DP classes
    Also, implemented some of Teacher routine.

---
+++ TemplateMethod/Non-DP/Teacher.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod.Non_DP
+{
+    class Teacher
+    {
+        public Teacher() { }
+
+        public void DoRoutine()
+        {
+            WakeUp();
+            MorningRoutine();
+            EatBreakfast();
+            GoToWork();
+        }
+
+        private void WakeUp()
+        {
+            Console.WriteLine($"{this} @ 5:00 AM: Good morning!");
+        }
+
+        private void MorningRoutine()
+        {
+            Console.WriteLine($"{this} @ 5:15 AM: Take a shower and brush your teeth.");
+        }
+
+        private void EatBreakfast()
+        {
+            Console.WriteLine($"{this} @ 5:50 AM: Have a nice English muffin and out the door.");
+        }
+
+        private void GoToWork()
+        {
+            Console.WriteLine($"{this} @ 6:00 AM: Yo, sound the bell, school's in sucka.");
+        }
+
+        private void Work()
+        {
+            Console.WriteLine($"{this} @ 8:00 AM: Reading, Writing, 'Rithmetic.");
+            Console.WriteLine($"{this} @ 12:00 PM: Lunchtime!");
+        }
+
+        private void GoHome()
+        {
+            Console.WriteLine($"{this} @ 3:00 PM: ");
+        }
+
+        public override string ToString()
+        {
+            return "Teacher";
+        }
+    }
+}
